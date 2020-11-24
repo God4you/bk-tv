e7e3e7bf71046ad69198cbd3d40cf47e0abce0f4
Benjamin Kakar <j0h@example.com>
2020-11-24 22:07:31 +0100

    title change

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -22,8 +22,6 @@ console.log(movie)
 function truncate(str, n){
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 }
-
-
     return (
         <header className="banner" style={{
             backgroundSize:"cover",
